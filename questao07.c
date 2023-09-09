279eb023d31915ee56d2623325525e5f4f42098b
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-08 21:43:03 -0300

    Update questao07.c

--- questao07.c
+++ questao07.c
@@ -1,3 +1,5 @@
+//7) Faça um programa que leia o número do funcionário, o número de horas trabalhadas mensais, o valor que recebe por hora e o número de filhos
+// com idade menor de 14 anos. Calcular e escrever o salário deste funcionário, sendo que cada filho menor de 14 anos acrescenta 10% do salário.
 #include <stdio.h>
 
 int main() {
