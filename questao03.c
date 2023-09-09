d9ba8d99f81d8cf38d43db24d3f3dca96353b523
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-08 21:26:07 -0300

    Add files via upload

---
+++ questao03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, num4, num5;
+    int soma, produto;
+    float media;
+
+    printf("Digite cinco números inteiros separados por espaços:\n");
+    scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
+
+    soma = num1 + num2 + num3 + num4 + num5;
+
+    produto = num1 * num2 * num3 * num4 * num5;
+
+    media = (float)(soma) / 5.0;
+
+    printf("Soma dos números: %d\n", soma);
+    printf("Produto dos números: %d\n", produto);
+    printf("Média dos números: %.2f\n", media);
+
+    return 0;
+}
