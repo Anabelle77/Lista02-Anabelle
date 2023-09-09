6af8dfc9f90e78ed2589227854b4a6da9a95250d
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-08 21:23:46 -0300

    Add files via upload

---
+++ questao01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int anos, meses, dias;
+    printf("Digite a idade em anos, meses e dias:\n");
+    scanf("%d %d %d", &anos, &meses, &dias);
+
+    int idadeEmDias = anos * 365 + meses * 30 + dias;
+
+    printf("A idade em dias é: %d dias\n", idadeEmDias);
+
+    return 0;
+}
