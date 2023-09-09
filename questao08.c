5acba866a779ec3d3caea53ce8a55dd8c258093c
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-08 21:49:03 -0300

    Update questao08.c

--- questao08.c
+++ questao08.c
@@ -1,13 +1,21 @@
-//8. Lê o saldo de uma aplicação e imprima o novo saldo, considerado o reajuste de 1%.
-#include<stdio.h>
-int main(void){
-  int saldo, reajuste;
-  
-  printf("Insira seu saldo: ");
-  scanf("%d",&saldo);
+//8) O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do distribuidor e dos impostos 
+//(aplicados ao custo de fábrica).Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%, 
+//escrever um programa em C que leia o custo de fábrica de um carro e escreva o custo ao consumidor.
+#include <stdio.h>
 
-  reajuste = saldo + saldo*0.01;
+int main() {
+    float custoFabrica, custoDistribuidor, custoConsumidor;
 
-  printf("O novo saldo é de: %d",reajuste);
-  return 0;
+    printf("Digite o custo de fábrica do carro: R$ ");
+    scanf("%f", &custoFabrica);
+
+    custoDistribuidor = 0.28 * custoFabrica;
+
+    float custoImpostos = 0.45 * custoFabrica;
+
+    custoConsumidor = custoFabrica + custoDistribuidor + custoImpostos;
+
+    printf("O custo ao consumidor é: R$ %.2f\n", custoConsumidor);
+
+    return 0;
 }
